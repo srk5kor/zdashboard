05aa172ed586d2be1690cbe5baec933f559efdc6
srk5kor <srinivas.kota18@gmail.com>
2022-11-12 16:39:15 +0000

    initial commit

--- project1/webapp/controller/View1.controller.js
+++ project1/webapp/controller/View1.controller.js
@@ -9,6 +9,16 @@ sap.ui.define([
 
         return Controller.extend("project1.controller.View1", {
             onInit: function () {
+                
+                var sServiceUrl = "/sap/opu/odata/sap/EPM_REF_APpS_PO_APV_SRV/"
+
+                var oModel = new sap.ui.model.odata.ODataModel(sServiceUrl)
+
+                this.getView().setModel(oModel)
+
+                var oCRMModel = new sap.ui.model.json.JSONModel("/sap/opu/odata/sap/EPM_REF_APpS_PROD_MAN_SRV/Products")
+
+                this.getView().byId("crmcontainer").setModel(oCRMModel, "oCRMModel")
 
             }
         });
